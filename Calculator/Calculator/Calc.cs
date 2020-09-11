473071c7d55d3ee32d012616e6e49bf16714125d
voda2600@mail.ru <voda2600@mail.ru>
2020-09-11 21:36:49 +0300

    calc ready

---
+++ Calculator/Calculator/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    public static class Calc
+    {
+        public static double Calculate(double a, char c, double b)
+        {
+            double ans = 0;
+            switch (c)
+            {
+                case '+': ans = a + b;break;
+                case '-': ans = a - b;break;
+                case '*': ans = a * b; break;
+                case '/': 
+                        {
+                        if (b == 0) throw new DivideByZeroException();
+                        else {
+                            ans = (double)a / b;
+                            break;
+                            }
+                        }
+                default:
+                    throw new Exception("Калькулятор не может работать с таким выражением");
+            }
+            return ans;
+
+        }
+    }
+}
+
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a your number -> operation -> number");
+            double a = GetNum();
+            char c = GetOper();
+            double b = GetNum();
+            while (true)
+            {
+                a = Calc.Calculate(a, c, b);
+                Console.WriteLine("You can continue the calculation. Enter the operation and the number...Answer = \n" + a);
+                c = GetOper();
+                b = GetNum();
+            }
+
+
+        }
+
+        private static char GetOper()
+        {
+            return char.Parse(Console.ReadLine());
+        }
+
+        private static int GetNum()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+    }
+}
