55f4e2cb326ad5ec94b7f26650f8afb7011db6a0
MrRobot <voda2600@mail.ru>
2020-09-28 16:20:45 +0300

    FCharp_Calc_NoTests

---
+++ CalculatorF#/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let divide x y = x/y
+let sum x y = x+y
+let difinition x y = x-y
+let multi x y = x*y
+
+let calculate op x y =
+    match op with 
+    |"+" -> divide x y
+    |"-" -> difinition x y
+    |"*" -> multi x y
+    |"/" -> divide x y
+    | _ -> raise(System.NotImplementedException("NotCompFunc"))
+
+
+[<EntryPoint>]
+let main argv =
+    let x  = Console.ReadLine() |> Int32.Parse
+    let op  = Console.ReadLine() 
+    let y  = Console.ReadLine() |> Int32.Parse
+    Console.WriteLine(calculate op x y)
+    0 // return an integer exit code
