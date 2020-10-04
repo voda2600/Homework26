9fa9a11c45987874511b89ceb2f5effa6ecdd0b2
MrRobot <voda2600@mail.ru>
2020-10-04 22:44:10 +0300

    CalcTestReady

--- CalcFTest/Tests.fs
+++ CalcFTest/Tests.fs
@@ -12,12 +12,22 @@ let ``divide 6/0`` () =
 [<Fact>]
 let ``calculate 5+5`` () =
     Assert.Equal(Some(10), Program.calculate "+" 5 5)
+[<Fact>]    
 let ``calculate 5*5`` () =
     Assert.Equal(Some(25), Program.calculate "*" 5 5)
+[<Fact>]
 let ``calculate 5/5`` () =
     Assert.Equal(Some(1), Program.calculate "/" 5 5)
+[<Fact>]
 let ``calculate 5-5`` () =
     Assert.Equal(Some(0), Program.calculate "-" 5 5)
+[<Fact>]
 let ``calculate "&" 5 5`` () =
     Assert.Equal(None, Program.calculate "&" 5 5)
+[<Fact>]
+let ``Console write 5`` () =
+    Assert.Equal(Program.write(Some 5), System.Console.Write(5))
 
+[<Fact>]
+let ``Console write None`` () =
+    Assert.Equal(Program.write(None), System.Console.Write("None"))
