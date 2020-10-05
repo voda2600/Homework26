f800b6e27bf03f4726de2e79903500db6124d7f2
MrRobot <voda2600@mail.ru>
2020-10-05 17:34:47 +0300

    mandelIsReadyv2

--- Mandelshtorn/ConsoleApplication1/Program.fs
+++ Mandelshtorn/ConsoleApplication1/Program.fs
@@ -8,8 +8,6 @@ open System.Drawing
 
 let height = 280.0
 let width = 280.0
-let cMax = complex 1.0 1.0
-let cMin = complex -1.0 -1.0
 let rec isInMandelbrotSet (zero, c, cur_iter, iter) =
     if cur_iter < iter && Complex.Abs(zero)<=2.0 then
         isInMandelbrotSet ( ((zero * zero) + c), c, cur_iter+1, iter )
@@ -20,8 +18,8 @@ let newCoordinates(x:int, y:int, scale:float, mx:float,my:float) =
 
 let colorize c =
     let r = (4 * c) % 255
-    let g = (6 * c) % 255
-    let b = (8 * c) % 255
+    let g = (1 * c) % 255
+    let b = (2 * c) % 255
     Color.FromArgb(r,g,b)
 
 
@@ -36,14 +34,14 @@ let createImage (mx, my, iter,scale) =
                 image.SetPixel(x,y, colorize( count ) )
     image
 //координаты точки приближения взял у сокомандника
-let mutateImage (ans:Form) (i:float)= ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,30+Convert.ToInt32(i*4.0),i*2.2), 0, 0))
+let mutateImage (ans:Form) (i:float)= ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.710,0.285,70+Convert.ToInt32(i*4.0),i*2.2), 0, 0))
 let ans  = new Form()
-ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,100,1.0), 0, 0))
+ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.710,0.28,100,1.0), 0, 0))
 ans.Size <- new Size(Convert.ToInt32(width), Convert.ToInt32(height))
 
 for i in 1.0..30.0 do
     mutateImage ans i
-
+ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.710,0.285,1000,30.0*2.2), 0, 0))
 
 do Application.Run(ans)
 
