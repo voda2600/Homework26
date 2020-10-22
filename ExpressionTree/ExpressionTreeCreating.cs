0f758c191282e26fb8de5631222685afe906dfe3
MrRobot <voda2600@mail.ru>
2020-10-22 17:09:41 +0300

    20%ready

---
+++ ExpressionTree/ExpressionTreeCreating.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ExpressionTree
+{
+    public static class ExpressionTreeCreating
+    {
+        private static Dictionary<char, Func<Expression, Expression, Expression>> mas =
+            new Dictionary<char, Func<Expression, Expression, Expression>>
+                {
+                    { '+', (current, next) => Expression.Add(current, next) },
+                    { '-', (current, next) => Expression.Subtract(current, next) },
+                    { '*', (current, next) => Expression.Multiply(current, next) },
+                    { '/', (current, next) => Expression.Divide(current, next) }
+                };
+
+        public static decimal Parsing(string expression)
+        {
+            foreach (var operation in mas)
+            {
+                if (expression.Contains(operation.Key))
+                {
+                    var parts = expression.Split(operation.Key);
+                    Expression result = Expression.Constant(Parsing(parts[0]));
+
+                    for (int i = 1; i < parts.Length; i++)
+                    {
+                        result = operation.Value(result, Expression.Constant(Parsing(parts[i])));
+                    }
+
+                    var lambda = Expression.Lambda<Func<decimal>>(result);
+                    var compiled = lambda.Compile();
+                    return compiled();
+                }
+            }
+
+            decimal value = 0;
+            decimal.TryParse(expression, out value);
+            return value;
+        }
+    }
+}
---
+++ ExpressionTree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExpressionTreeCreating.Parsing("2+3*10"));
+        }
+    }
+}
